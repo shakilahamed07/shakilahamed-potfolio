6cbaf35bb79f45d8fda2cf9ee5829fc4fb128024
shakilahamed07 <shakil200607@gmail.com>
2025-12-30 23:43:20 +0600

    nevbar update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,33 +6,48 @@ import { FaDownload } from "react-icons/fa";
 import { ImCross } from "react-icons/im";
 
 const Navbar = () => {
+  const [value, setValue] = useState(true);
 
-  const [value, setValue] = useState(true)
-  
   const links = (
     <>
       <li>
-        <a href="#home" className="mb-2 md:mb-0 font-medium hover:bg-[#2e2e2e] focus:text-primary">
+        <a
+          href="#home"
+          className="mb-2 md:mb-0 font-medium hover:bg-[#2e2e2e] focus:text-primary"
+        >
           Home
         </a>
       </li>
       <li>
-        <a href="#about-me" className="mb-2 md:mb-0 font-medium hover:bg-[#2e2e2e] focus:text-primary">
+        <a
+          href="#about-me"
+          className="mb-2 md:mb-0 font-medium hover:bg-[#2e2e2e] focus:text-primary"
+        >
           About
         </a>
       </li>
       <li>
-        <a href="#skills" to="/" className="mb-2 md:mb-0 font-medium hover:bg-[#2e2e2e] focus:text-primary">
-         Skills
+        <a
+          href="#skills"
+          to="/"
+          className="mb-2 md:mb-0 font-medium hover:bg-[#2e2e2e] focus:text-primary"
+        >
+          Skills
         </a>
       </li>
       <li>
-        <a href="#projects" className="mb-2 md:mb-0 font-medium hover:bg-[#2e2e2e] focus:text-primary">
-         Projects
+        <a
+          href="#projects"
+          className="mb-2 md:mb-0 font-medium hover:bg-[#2e2e2e] focus:text-primary"
+        >
+          Projects
         </a>
       </li>
       <li>
-        <a href="#contactUs" className="mb-2 md:mb-0 font-medium hover:bg-[#2e2e2e] focus:text-primary">
+        <a
+          href="#contactUs"
+          className="mb-2 md:mb-0 font-medium hover:bg-[#2e2e2e] focus:text-primary"
+        >
           Contact Us
         </a>
       </li>
@@ -40,27 +55,54 @@ const Navbar = () => {
   );
 
   return (
-    <div  className="bg-linear-to-t from-[#1b1a1a] to-secondary shadow-2">
-      <div className="navbar  text-white max-w-[1350px] mx-auto py-0 z-50">
-        <div className="navbar-start">
+    <div className="bg-linear-to-t from-[#1b1a1a] to-secondary shadow-2">
+      <div className="navbar flex justify-between text-white max-w-[1350px] mx-auto py-0 z-50">
+        {/* <div className="">
+          <a
+            href="https://drive.google.com/file/d/1HvP1BCNyrdePI0YLwCYFweqEsE3iKbUg/view?usp=drive_link"
+            target="blank"
+            className="btn btn-primary bg-primary px-3 font-bold text-white border-none rounded-xl hover:bg-[#44a158] hover:scale-95 transition-all"
+          >
+            <FaDownload />
+            Resume
+          </a>
+        </div> */}
+        <img className="w-8 ml-3 mb-5 mt-5 lg:hidden" src={logo} alt="" />
+        <div className="">
           <div className="dropdown">
-            <div onClick={()=>{setValue(true)}}>
-              <div
-              tabIndex={0}
-              role="button"
-              className="lg:hidden  text-white"
+            <div
+              onClick={() => {
+                setValue(true);
+              }}
             >
-              <IoMenu size={33} />
-            </div>
+              <div tabIndex={0} role="button" className="lg:hidden  text-white">
+                <IoMenu size={33} />
+              </div>
             </div>
             <ul
               tabIndex={0}
-              className={`${value ? 'block' : 'hidden'} menu dropdown-content z-1 -ml-3 w-[200px] min-h-[calc(100vh-0px)] shadow-2xl -mt-12 bg-linear-to-t from-[#1b1a1a] to-secondary text-white`}
+              className={`${
+                value ? "block" : "hidden"
+              } menu dropdown-content z-1 -ml-40 w-[200px] min-h-[calc(100vh-0px)] shadow-2xl -mt-11 bg-linear-to-t from-[#1b1a1a] to-secondary text-white`}
             >
-              <div className="flex items-center justify-between">
-                <img className="w-8 ml-3 mb-5 mt-2 " src={logo} alt="" />
-                <span onClick={()=>{setValue(false)}} className="mb-2 mr-5">
-                  <ImCross className="hover:scale-110 transition-all"/>
+              <div className="flex items-center justify-between pb-5">
+                <div className="pt-1 ml-2">
+                  <a
+                    href="https://drive.google.com/file/d/1HvP1BCNyrdePI0YLwCYFweqEsE3iKbUg/view?usp=drive_link"
+                    target="blank"
+                    className="flex items-center justify-between gap-1 btn-primary bg-primary px-1.5 py-1.5 text-xs text-white border-none rounded-xl hover:bg-[#44a158] hover:scale-95 transition-all"
+                  >
+                    <FaDownload />
+                    Resume
+                  </a>
+                </div>
+                <span
+                  onClick={() => {
+                    setValue(false);
+                  }}
+                  className=" mr-3"
+                >
+                  <ImCross className="hover:scale-110 transition-all" />
                 </span>
               </div>
               {links}
@@ -70,12 +112,6 @@ const Navbar = () => {
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">{links}</ul>
-        </div>
-        <div className="navbar-end">
-          <a href="https://drive.google.com/file/d/1HvP1BCNyrdePI0YLwCYFweqEsE3iKbUg/view?usp=drive_link" target="blank" className="btn btn-primary bg-primary px-3 font-bold text-white border-none rounded-xl hover:bg-[#44a158] hover:scale-95 transition-all">
-            <FaDownload />
-            Resume
-          </a>
         </div>
       </div>
     </div>
